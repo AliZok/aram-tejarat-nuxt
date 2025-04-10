ce417f814f92f1a29920246e4567ed2a3ce22394
freemanzok <ali.zokaei.1367@gmail.com>
2025-04-10 17:52:54 +0330

    ready

--- app.vue
+++ app.vue
@@ -2,19 +2,27 @@
   <div class="app">
     <div class="my-container">
       <div class="my-header">
-        <div>
+        <div class="">
+          آرام تجارت
+        </div>
+        <div class="flex mr-2">
           <img class="logo" src="public/images/logo.png" alt="">
         </div>
       </div>
       <div class="coworkers">
         <div>
-          <img src="public/images/four.svg" alt="">
-          <div>
+          <img class="coworkers-image" src="public/images/four.svg" alt="">
+          <div class="relative inline-block">
             <div class="mb-2">
               در حال به روز رسانی هستیم
             </div>
             <div>
               لطفا شکیبا باشید
+            </div>
+            <div class="gears">
+              <img class="gear1 rotating" src="public/images/gear1.png" alt="">
+              <img class="gear2 rotating" src="public/images/gear1.png" alt="">
+              <img class="gear3 rotating" src="public/images/gear1.png" alt="">
             </div>
           </div>
         </div>
@@ -26,7 +34,11 @@
 body {
   margin: 0;
   font-family: yekan;
-  color: #516d7c;
+  color: #b0883e;
+  background: rgb(28, 28, 28);
+}
+.flex{
+  display: flex;
 }
 
 .my-container {
@@ -37,11 +49,48 @@ body {
   text-align: center;
   height: 100vh;
 }
-
+.mr-2{
+  margin-right: 10px;
+}
 .my-header {
-  text-align: left;
-  padding: 5px;
-  margin-bottom: 25px;
+  text-align: right;
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  border-bottom: solid 1px #b0883e;
+  padding: 6px 0;
+  background: rgb(0, 0, 0);
+
+}
+.gears{
+  position: absolute;
+  right: -30px;
+  top: 0;
+  opacity: 0.5;
+}
+.gear1{
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  top: -10px;
+  right: -20px;
+}
+.gear2{
+  position: absolute;
+  top: -20px;
+  right: -45px;
+  width: 20px;
+  height: 20px;
+}
+.gear3 {
+  position: absolute;
+  top: 10px;
+  right: -52px;
+  width: 30px;
+  height: 30px;
 }
 
 .logo {
@@ -56,8 +105,10 @@ body {
   align-items: center;
   padding: 0 10px;
 }
-
-.coworkers img {
+.relative{
+  position: relative;
+}
+.coworkers-image {
   height: 420px;
   width: 100%;
   margin-bottom: 30px;
@@ -66,6 +117,25 @@ body {
 .mb-2{
   margin-bottom:10px ;
 }
+
+.inline-block{
+  display: inline-block;
+}
+
+/* Rotate Animation */
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.rotating {
+  animation: rotate 5s linear infinite; 
+}
+
 
 @font-face {
   font-family: yekan;
