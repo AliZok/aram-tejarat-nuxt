2ca7fa9b5dad341088d4df9611ca4bb6056b9f9b
freemanzok <ali.zokaei.1367@gmail.com>
2025-04-11 16:12:54 +0330

    change 35

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon/arm-logo.png",
+        },
+    
+      ],
+    },
+  },
 })
